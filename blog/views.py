439f55b5a4d6f46d43b0542153263870bda88090
MaximShaybakov <momon_boton@mail.ru>
2023-05-12 14:11:42 +0500

    Added views posts as class

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 
 
@@ -24,3 +26,11 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    """ Posts list """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
