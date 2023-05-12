9085b89041bfd88665b23d964f45ed9411728da8
MaximShaybakov <momon_boton@mail.ru>
2023-05-12 12:01:47 +0500

    Create views for 'blog' app
    Added views for all blogs and blogs by author.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, user_id: int):
+    post = get_object_or_404(Post, id=user_id, status=Post.Status.PUBLISHED)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
