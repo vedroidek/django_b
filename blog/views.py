25180980c2d9e6eaf1cc736b8ccf45d3040f7422
MaximShaybakov <momon_boton@mail.ru>
2023-05-12 13:05:07 +0500

    Added 'get_absolute_url' for post.
    Change in list.html, models.py, urls.py, views.py.

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,11 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, user_id: int):
-    post = get_object_or_404(Post, id=user_id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
