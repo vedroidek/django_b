e1e4571c9e67ac8e6d9c79ab6294a84f3f9ae4c4
MaximShaybakov <momon_boton@mail.ru>
2023-05-12 10:54:39 +0500

    Created the first migration

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
-    author = models.ForeignKey('User', on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
