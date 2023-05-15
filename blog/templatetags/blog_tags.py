3efc9a960aea48b9b316251ca2bf02d40c8685cf
MaximShaybakov <momon_boton@mail.ru>
2023-05-16 00:35:10 +0500

    Added show most commented posts (5)

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -10,6 +12,12 @@ def total_posts():
 
 
 @register.inclusion_tag('blog/post/latest_posts.html')
-def show_latest_posts(count=5):
+def show_latest_posts(count: int = 5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count: int = 5):
+    return Post.published.annotate(
+        total_comments=Count('comments')).order_by('-total_comments')[:count]
